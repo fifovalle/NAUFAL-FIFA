2faf618ff3e9012f80f5d3244183233688e02372
fifovalle <fifanaufal10@gmail.com>
2025-09-08 16:57:53 +0700

    feat: menambahkan bulan di journey

--- constants/journey.ts
+++ constants/journey.ts
@@ -1,30 +1,30 @@
 export const journey = [
     {
-        years: "2025 - Present",
-        role: "Fullstack Web Developer",
-        institution: "PT. Spasial Teknologi Indonesia",
+        years: "Jul 2025 - Present",
+        role: "Fullstack Developer",
+        institution: "PT. Indonesia Tekno Kreatif",
         gallery: [],
     },
     {
-        years: "2025 - Present",
+        years: "Jun 2025 - Sep 2025",
         role: "Team Leader Frontend Web Developer",
         institution: "PT. Minilemon Nusantara",
         gallery: [],
     },
     {
-        years: "2025 - Present",
+        years: "May 2025 - Sep 2025",
         role: "Senior Frontend Mobile Developer",
         institution: "PT. Minilemon Nusantara",
         gallery: [],
     },
     {
-        years: "2025 - 2025",
+        years: "Feb 2025 - Jul 2025",
         role: "Frontend Mobile Developer",
         institution: "PT. Minilemon Nusantara",
         gallery: ["/assets/journey/minilemon-certificate.jpeg"],
     },
     {
-        years: "2025 - 2025",
+        years: "Feb 2025 - Jul 2025",
         role: "Project Manager",
         institution:
             "Indonesian Agency for Meteorology, Climatology, and Geophysics (BMKG)",
@@ -33,7 +33,7 @@ export const journey = [
 
     // --- 2024 ---
     {
-        years: "2024 - Present",
+        years: "Jan 2024 - Present",
         role: "Fullstack Cross Platform Developer",
         institution:
             "Indonesian Agency for Meteorology, Climatology, and Geophysics (BMKG)",
@@ -46,13 +46,13 @@ export const journey = [
         ],
     },
     {
-        years: "2024 - 2024",
+        years: "Aug 2024 - Sep 2024",
         role: "Fullstack Web Developer",
         institution: "Universitas Jenderal Achmad Yani (Unjani)",
         gallery: [],
     },
     {
-        years: "2024 - 2024",
+        years: "Jun 2024 - Jul 2024",
         role: "Fullstack Web Developer",
         institution: "Basarnas Bengkulu",
         gallery: [
@@ -63,7 +63,7 @@ export const journey = [
 
     // --- 2022 ---
     {
-        years: "2022 - Present",
+        years: "Sep 2022 - Present",
         role: "Student in Informatics",
         institution: "Universitas Jenderal Achmad Yani (Unjani)",
         gallery: [],
